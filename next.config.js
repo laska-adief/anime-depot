7234beaa82aafb4f52d4788a180ce4a7c31fb334
laska-adief <laskaadief.la@gmail.com>
2023-11-28 08:53:44 +0700

    config: move image config in experimental

--- next.config.js
+++ next.config.js
@@ -1,14 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
-  images: {
-    unoptimized: true,
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "*",
-      },
-    ],
+  experimental: {
+    images: {
+      unoptimized: true,
+      remotePatterns: [
+        {
+          protocol: "https",
+          hostname: "*",
+        },
+      ],
+    },
   },
 };
 
