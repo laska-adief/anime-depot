3d00c09ab88768b3bde3ee29b2dd5fecf649cfe5
laska-adief <laskaadief.la@gmail.com>
2023-11-28 07:54:23 +0700

    fix: add Headers in action fetch

--- src/app/action.ts
+++ src/app/action.ts
@@ -1,11 +1,11 @@
 "use server";
 
-import fetch, { RequestInit } from "node-fetch";
+import fetch, { Headers } from "node-fetch";
 
 export const fetchAnime = async (page: number, limit: number = 12) => {
-  const headers: RequestInit["headers"] = {
+  const headers = new Headers({
     "Content-Type": "application/json",
-  };
+  });
   const response = await fetch(`https://shikimori.one/api/animes?page=${page}&limit=${limit}&order=popularity`, { headers: headers });
   const data = await response.json();
 
