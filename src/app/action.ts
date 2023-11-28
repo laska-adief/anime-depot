f7256fae5b6ce6933cbd52af0e6d4903d97237b2
laska-adief <laskaadief.la@gmail.com>
2023-11-28 07:50:56 +0700

    fix: add header in fecth using node-fetch

--- src/app/action.ts
+++ src/app/action.ts
@@ -1,7 +1,12 @@
 "use server";
 
+import fetch, { RequestInit } from "node-fetch";
+
 export const fetchAnime = async (page: number, limit: number = 12) => {
-  const response = await fetch(`https://shikimori.one/api/animes?page=${page}&limit=${limit}&order=popularity`);
+  const headers: RequestInit["headers"] = {
+    "Content-Type": "application/json",
+  };
+  const response = await fetch(`https://shikimori.one/api/animes?page=${page}&limit=${limit}&order=popularity`, { headers: headers });
   const data = await response.json();
 
   return data;
